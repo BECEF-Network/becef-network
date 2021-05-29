3742a97519085da853be3367bd07583e4509fe86
reve23 <78494847+reve23@users.noreply.github.com>
2021-05-29 17:58:49 +0600

    Add files via upload

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,23 @@
 import react from 'react';
+import styled from 'styled-components';
+import HomeSlider from '../components/HomeSlider';
 
 const Homepage = () => {
     return (
-        <div>homepage</div>
+        <>
+        <Container>
+            <HomeSlider />
+        </Container>
+        </>
     );
 };
+const Container = styled.main`
+position:relative;
+min-height:calc(100vh - 250px);
+overflow-x: hidden;
+display: block;
+top:22px;
+padding: 0 calc(3.5vw - 5px);
+background: black;
+`;
 export default Homepage;
